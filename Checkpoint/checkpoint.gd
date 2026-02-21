26cf6c4b80bd95af3ea959a9afc5cb09ef3bb0c8
Sutams <ernestogarciaa1@gmail.com>
2026-02-21 14:38:47 -0300

    aseprite plugin

--- Checkpoint/checkpoint.gd
+++ Checkpoint/checkpoint.gd
@@ -1,9 +1,12 @@
 extends Area2D
 
+@onready var animation = $AnimatedSprite2D
+
 var active : bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	animation.play("inactive")
 	pass # Replace with function body.
 
 
@@ -15,5 +18,5 @@ func _process(_delta: float) -> void:
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
 		active = true
-		$Sprite2D.modulate = Color.GREEN
+		animation.play("active")
 	pass # Replace with function body.
